4b4ec00c613bb7043c29ffcbdfc72d59aeea63f7
Kaio Silva <jkaio37@hotmail.com>
2023-10-02 16:39:45 -0400

    Initial commit

---
+++ src/com/Calc/Calc.java
@@ -0,0 +1,30 @@
+package com.Calc;
+
+import java.util.Scanner;
+public class Calc {
+	static public void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		Numero n1 = new Numero();
+		Numero n2 = new Numero();
+		Numero res = new Numero();
+		
+		String opc = "S";
+		
+		while(opc.equals("S") || opc.equals("s")) {
+			System.out.printf("%nDigite o valor 1: ");
+			n1.setValor(scan.nextInt());
+			
+			System.out.printf("%nDigite o valor 2: ");
+			n2.setValor(scan.nextInt());
+			
+			res.setValor(n1.getValor() + n2.getValor());
+			
+			System.out.printf("%n A soma de %d com %d e igual a %d", n1.getValor(), n2.getValor(), res.getValor());
+			System.out.printf("%n Deseja somar outro valor?");
+			opc=scan.next();
+
+		}
+	}
+
+}
